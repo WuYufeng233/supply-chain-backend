8d4ffd83d99139212e684b1543cf7e8b90336b5b
Yukino0812 <Yukino0812@qq.com>
2019-07-02 10:59:20 +0800

    移除一些代码

--- src/main/java/cn/edu/scut/sse/supply/general/controller/MainController.java
+++ src/main/java/cn/edu/scut/sse/supply/general/controller/MainController.java
@@ -1,6 +1,5 @@
 package cn.edu.scut.sse.supply.general.controller;
 
-import cn.edu.scut.sse.supply.general.entity.vo.ResponseResult;
 import cn.edu.scut.sse.supply.general.service.MainService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.File;
 
 /**
  * @author Yukino Yukinoshita
@@ -39,13 +36,6 @@ public class MainController {
     String testPost(@RequestParam String param) {
         logger.debug("param = {}", param);
         return param;
-    }
-
-    @RequestMapping("/test/path")
-    public @ResponseBody
-    ResponseResult testPath(@RequestParam String path) {
-        File file = new File(path);
-        return new ResponseResult().setCode(0).setMsg(file.exists() + "-" + file.getAbsolutePath());
     }
 
     @RequestMapping(value = "/get/blocknumber")
--- src/main/java/cn/edu/scut/sse/supply/util/ContractUtil.java
+++ src/main/java/cn/edu/scut/sse/supply/util/ContractUtil.java
@@ -10,11 +10,6 @@ package cn.edu.scut.sse.supply.util;
 
 public class ContractUtil {
 
-    public static final String CONTRACT_REPO_ADDRESS = "0x8c17cf316c1063ab6c89df875e96c9f0f5b2f744";
     public static final String ENTERPRISE_TOKEN_ADDRESS = "0x0b9ce0c6c4a85816bb328815d6befd7aa56119e8";
-    public static final String BANK_APPLICATION_ADDRESS = "0xdfdd3ada340d7346c40254600ae4bb7a6cd8e660";
-    public static final String INSURANCE_APPLICATION_ADDRESS = "0xf8bd0e4d6c1e8c1dc63ad4ae5af4a7d6a84c7107";
-    public static final String EXPRESS_APPLICATION_ADDRESS = "0x11ee26d4bc14b203c26c274cc3b0caff92e28222";
-    public static final String CARGO_RECEIVE_ADDRESS = "0xd730428f9dc97c8c480502d3601f2d9d284f92f2";
 
 }
